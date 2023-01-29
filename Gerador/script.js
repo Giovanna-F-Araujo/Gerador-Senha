683960e89b51ed5f1c9913af733c109fb476e8e4
Giovanna Ferreira Araujo <contatogiovanna223@gmail.com>
2023-01-29 12:03:00 -0300

    Correção de erros 1

--- Gerador/script.js
+++ Gerador/script.js
@@ -2,25 +2,27 @@ let sliderElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#button");
 
 let sizePassword = document.querySelector("#valor");
-let Password = document.querySelector("#password");
+let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789*!@$%#";
-let novaSenha = "";
+let charset = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789*!@$%# ';
+
+let novaSenha = '';
 
 sizePassword.innerHTML = sliderElement.value;
 
-sliderElement.oninput = function(){
-    sizePassword.innerHTML = this.value
-};
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
 
 function generatePassword(){
    let pass = "";
-   for(let i = 0, n = charset.length; i<sliderElement.valeu;i++){
-    pass += charset.charAt(math.floor(math.random()*n));
+   for(let i = 0, n = charset.length; i < sliderElement.valeu; i++){
+    pass += charset.charAt(Math.floor(Math.random() * n));
 
    }
+   console.log(pass)
    containerPassword.classList.remove("hide");
    password.innerHTML = pass;
    novaSenha = pass;
@@ -28,5 +30,5 @@ function generatePassword(){
 }
 function copyPassword(){
     alert("Senha copiada com sucesso!")
-    navigator.clipboard.writeText(novaSenha)
+    navigator.clipboard.writeText(novaSenha);
 }
